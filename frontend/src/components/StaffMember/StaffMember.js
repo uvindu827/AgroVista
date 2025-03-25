50af2798515b7494e8cf4ca45e53c06867de6743
uvindu abhisheka <dharmasenaua@gmail.com>
2025-03-26 01:25:05 +0530

    changed the design of the table

--- frontend/src/components/StaffMember/StaffMember.js
+++ frontend/src/components/StaffMember/StaffMember.js
@@ -12,14 +12,14 @@ function StaffMember({ StaffMember }) {
   } = StaffMember;
 
   return (
-    <tr className="hover:bg-gray-50">
-      <td className="px-6 py-4 whitespace-nowrap">{firstName}</td>
-      <td className="px-6 py-4 whitespace-nowrap">{lastName}</td>
-      <td className="px-6 py-4 whitespace-nowrap">{email}</td>
-      <td className="px-6 py-4 whitespace-nowrap">{phoneNumber}</td>
-      <td className="px-6 py-4 whitespace-nowrap">{address}</td>
-      <td className="px-6 py-4 whitespace-nowrap">{jobTitle}</td>
-      <td className="px-6 py-4 whitespace-nowrap">${basicSalary}</td>
+    <tr className="hover:bg-yellow-50 transition-colors">
+      <td className="px-6 py-4 whitespace-nowrap text-gray-700">{firstName}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-700">{lastName}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-700">{email}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-700">{phoneNumber}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-700">{address}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-700">{jobTitle}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-green-600 font-medium">${basicSalary}</td>
     </tr>
   );
 }
