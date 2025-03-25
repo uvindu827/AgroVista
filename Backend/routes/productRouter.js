46222347c77763eccd139740f66548d5e41ebe00
kavin1231 <chamathkaveendra@gmail.com>
2025-03-25 12:59:39 +0530

    successfully create product crud

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -1,18 +1,14 @@
-import express from "express";
-import {
-  addProduct,
-  deleteProduct,
-  getProducts,
-  updateProduct,
-} from "../controllers/productController.js";
+import express from 'express';
+import { addProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
-productRouter.post("/", addProduct);
+productRouter.post("/",addProduct)
+productRouter.get("/",getProducts)
+productRouter.put("/:key",updateProduct)
+productRouter.delete("/:key",deleteProduct)
+productRouter.get("/:key",getProduct)
 
-productRouter.get("/", getProducts);
 
-productRouter.put("/:key", updateProduct);
-productRouter.delete("/:key", deleteProduct);
 
 export default productRouter;
