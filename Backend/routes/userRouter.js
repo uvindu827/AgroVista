c41c3ec0c43499bdb9826d923c83f9d446c82521
kavin1231 <chamathkaveendra@gmail.com>
2025-03-25 16:09:58 +0530

    correct user router🥲

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -12,11 +12,11 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.post("/",registerUser)
+userRouter.post("/", registerUser);
 
-userRouter.post("/login",loginUser)
+userRouter.post("/login", loginUser);
 
-userRouter.get("/all",getAllUsers)
+userRouter.get("/all", getAllUsers);
 
 userRouter.put("/block/:email", blockOrUnblockUser);
 
@@ -27,14 +27,5 @@ userRouter.get("/sendOTP", sendOTP);
 userRouter.post("/verifyEmail", verifyOTP);
 
 userRouter.get("/", getUser);
-userRouter.post("/google",loginWithGoogle)
-
-userRouter.get("/sendOTP",sendOTP)
-
-userRouter.post("/verifyEmail",verifyOTP)
-
-userRouter.get("/",getUser)
-
-
 
 export default userRouter;
