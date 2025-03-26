3bdb6f31fde3816136a4af06f12b7832962c707a
kavin1231 <chamathkaveendra@gmail.com>
2025-03-26 12:44:22 +0530

    add register function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
-import Staff from './components/StaffDetails/Staff';
-import AddEmployee from './components/AddEmployee/AddEmployee';
-import {Route, Routes} from 'react-router';
+import React from "react";
+import "./App.css";
+import Staff from "./components/StaffDetails/Staff";
+import AddEmployee from "./components/AddEmployee/AddEmployee";
+import { Route, Routes } from "react-router";
+import RegisterPage from "./components/Register/register";
 
 function App() {
   return (
     <div>
       <React.Fragment>
         <Routes>
-          <Route path="/" element={<Staff />}/>
+          <Route path="/" element={<Staff />} />
           <Route path="/add-employee" element={<AddEmployee />} />
+          <Route path="/users/" element={<RegisterPage />} />
         </Routes>
       </React.Fragment>
     </div>
