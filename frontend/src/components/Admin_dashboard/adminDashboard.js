663647eab781c472af565d5a8f2b3ff03c89d37f
uvindu abhisheka <dharmasenaua@gmail.com>
2025-04-23 00:14:06 +0530

    implemented users interface of the newsfeed

--- frontend/src/components/Admin_dashboard/adminDashboard.js
+++ frontend/src/components/Admin_dashboard/adminDashboard.js
@@ -12,6 +12,10 @@ function AdminDashboard() {
     navigate("/nf-management");
   };
 
+  const handleNewsFeed = () => {
+    navigate("/newsfeed");
+  };
+
 return (
     <div className="px-4 sm:px-6 lg:px-8 py-8 bg-green-50 min-h-screen">
         <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none mb-4">
@@ -22,12 +26,20 @@ return (
                 Staff Management
             </button>
         </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none mb-4">
             <button
                 onClick={handlenfManagement}
-                className="inline-flex items-center justify-center rounded-md bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                className="inline-flex items-center justify-center rounded-md bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
             >
                 Newsfeed Management
+            </button>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none mb-4">
+            <button
+                onClick={handleNewsFeed}
+                className="inline-flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+                User Newsfeed
             </button>
         </div>
     </div>
